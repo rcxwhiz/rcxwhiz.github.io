c7cdf69d9ebfaeeeb025d83a6303e230232dc133
Josh Bedwell <rcxwhiz@gmail.com>
2025-03-28 15:22:41 -0700

    Redid website to make it significantly simpler

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,3 @@
-const { DateTime } = require("luxon");
-const sass = require("sass");
-const path = require("node:path");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
@@ -11,19 +8,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
 
     // Filters
-    eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
-        // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-        return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
-    });
-
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
-    });
-
-    eleventyConfig.addFilter('shortDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd MMM yy");
-    });
 
     const urlPattern = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
     eleventyConfig.addFilter('isLink', (linkStr) => {
@@ -32,28 +16,6 @@ module.exports = function (eleventyConfig) {
 
     // copy public files
     eleventyConfig.addPassthroughCopy("src/public");
-
-    // .scss processing
-    eleventyConfig.addTemplateFormats("scss");
-    eleventyConfig.addExtension("scss", {
-        outputFileExtension: "css",
-        compile: function (inputContent, inputPath) {
-            let parsed = path.parse(inputPath);
-            let result = sass.compileString(inputContent, {
-                loadPaths: [
-                    parsed.dir || ".",
-                    this.config.dir.includes
-                ],
-                style: "compressed"
-            });
-            return (data) => {
-                return result.css;
-            }
-        }
-    });
-
-    // ignore underscored scss files
-    eleventyConfig.ignores.add("**/_*.scss");
 
     // output dir
     return {
